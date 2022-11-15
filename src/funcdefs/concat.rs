0656871df33e99a00a8d5fa7f58c9153d7544cba
Tetsuya Takatsuru <tetsuya@taka2ru.jp>
2022-11-15 20:12:18 +0900

    fucdefs

---
+++ src/funcdefs/concat.rs
@@ -0,0 +1,7 @@
+pub trait Concat<T> {
+    fn concat_0(left: &[T], right: &[T]) -> Vec<T>;
+    fn concat_1(left: &Vec<T>, right: &Vec<T>) -> Vec<T>;
+    fn concat_2(left: Vec<T>, right: Vec<T>) -> Vec<T>;
+    fn concat_3<'a>(left: &'a [T], right: &'a [T]) -> &'a [T];
+    fn concat_4(left: &[T], right: &[T]) -> [T];
+}
---
+++ src/funcdefs/mod.rs
@@ -0,0 +1,2 @@
+pub mod reqres;
+pub mod concat;
